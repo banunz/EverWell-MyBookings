abe420f25753428bce7c21abd9d7286d168a1b25
Janu <banucres@gmail.com>
2024-08-08 18:48:06 +1200

    adding the file

---
+++ cypress/config.js
@@ -0,0 +1,29 @@
+export default {
+    dev: {
+        baseUrl: "https://dev.website.co.nz",
+        loginDomain: "login.dev.website.co.nz",
+        user: {
+            userName: "test1",
+            password: "test1234"
+        },
+        user2: {
+            userName: "test2",
+            Password: "test123"
+        }
+    },
+    test: {
+        baseUrl: "https://test.website.co.nz",
+        loginDomain: "login.dev.website.co.nz",
+        user: {
+            userName: "test1",
+            password: "test1234"
+        },
+        user2: {
+            userName: "test2",
+            Password: "test123"
+        }
+    }
+
+
+
+}
